5d3f45bba656fe9ee86826911865509052d5092f
Takayuki Muranushi <muranushi@gmail.com>
2016-10-17 12:52:49 +0900

    Create 12-min sampled goes lightcurve

--- sample-goes-lightcurve.py
+++ sample-goes-lightcurve.py
@@ -3,10 +3,14 @@
 import datetime
 from observational_data import *
 
+dt = datetime.timedelta(minutes=12)
+
+set_data_path('data')
+
 with open("goes-data-12min.txt","w") as fp:
-    for step in range(5*366*24*5):
-        t = datetime.datetime(2011,1,1,0,0) + datetime.timedelta(minutes=12*step)
-        x = max(1e-8, get_goes_max(t, datetime.timedelta(hours=1)))
+    for step in range(6*366*24*5):
+        t = datetime.datetime(2011,1,1,0,0) + dt * step
+        x = max(1e-8, get_goes_max(t, dt))
         tstr = t.strftime("%Y-%m-%dT%H:%M")
         if step % 120 == 0:
             print tstr,x
